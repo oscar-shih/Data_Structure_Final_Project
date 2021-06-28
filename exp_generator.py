7c969c56e6ed2c88287785b42e4c7dc523d4a33f
Beer-and-Skittles <jessicahu1012@gmail.com>
2021-06-28 11:08:44 +0800

    update from yesterday!

--- exp_generator.py
+++ exp_generator.py
@@ -1,6 +1,6 @@
 import random
 import time
-import parse_tree as ps
+import parse_tree2 as ps
 import shunting_yard as sy
 
 class Generator:
@@ -11,28 +11,31 @@ class Generator:
     def genExp(self,length):
         exp = ""
         for i in range(length//2):
-            exp += self.nums[random.randint(0,1000)%9]
-            exp += self.oprs[random.randint(0,1000)%4]
-        exp += self.nums[random.randint(0,1000)%9]
+            exp += self.nums[random.randint(0,8)]
+            exp += self.oprs[random.randint(0,3)]
+        exp += self.nums[random.randint(0,8)]
         return exp
 
 
 if __name__ == '__main__':
 
+    # inputs: m mathematical expressions each with length l
     test = Generator()
     inputs = []
-    for i in range(100):
-        inputs.append(test.genExp(100))
+    m,l = 100,100
+    for i in range(m):
+        inputs.append(test.genExp(l))
     
+    # time comparison
     t1 = time.time()
-    ps_tree = ps.Calculator1()
+    ps_tree = ps.Calculator2()
     for exp in inputs:
         ps_tree.calculate(exp)
     t2 = time.time()
     print("Parse Tree: \t",t2-t1)
 
     t3 = time.time()
-    shunt_yrd = sy.Calculator2()
+    shunt_yrd = sy.Calculator3()
     for exp in inputs:
         shunt_yrd.calculate(exp)
     t4 = time.time()
--- parse_tree.py
+++ parse_tree.py
@@ -1,24 +1,26 @@
-# Reference: 7.6 Parse Tree (Pythonds)
-#            https://runestone.academy/runestone/books/published/pythonds/Trees/ParseTree.html
-# 
-# Input:    expression in type string; 
+# Input:    mathematical expression in type string; 
 #           with operators and functions expressed in corresponding notations 
 #           (see page bottom)
 # Output:   type float
-# ToDo:     index feature
+# ToDo:     1. parse tree traversal (combining nodes)
+#           2. calculation for functions and operators
+#           2. conditional statement for functions in rank
+#           3. decimal numbers
+
+
 
 import numpy as np
 import math
 import time
 
-class Node:
+class Node1:
     def __init__(self, c):
         self.content = c
         self.parent  = None
         self.r_child = None
         self.l_child = None
 
-class ParseTree:
+class ParseTree1:
     def __init__(self, a):
         self.angle = a
         self.nums = "0123456789."
@@ -27,200 +29,70 @@ class ParseTree:
         self.ops = {'(':-1, '+':2, '-':2, '*':3, '/':3, '^':4, 'c':5, 'd':5,\
                     'e': 5, 'f':5, 'g':5, 'h':5, 'i':5, 'j':5, 'k':5, 'l':5,\
                     'm': 5, 'n':5, 'o':5, 'p':5, 'q':5, 'r':5, 's':5 }
-        self.root = Node(None)
+        self.root = Node1(None)
 
 
-    def toNum(self, char):
-        if(char == 'a'):    # pi
-            return np.pi
-        if(char == 'b'):    # e
-            return np.exp(1)
-
-
-    def buildTree(self, expr):
-        
-        self.root.l_child = Node(None)   # start from left child of root
-        self.root.l_child.parent = self.root
-        current = self.root.l_child
-        
-        i = 0
-        while i < len(expr):
+    def rank(self,expr):
+        rk = []
+        in_par = 0
+        for i in range(len(expr)):
             char = expr[i]
-            if(char in self.nums):
-                temp = ""
-                while(i < len(expr) and expr[i] in self.nums):
-                    temp += str(expr[i])
-                    i += 1
-                i = i-1
-                current.content = float(temp)
-                current = current.parent
-        
-            elif(char in self.codes):
-                current.content = self.toNum(char)
-                current = current.parent
-
-            elif(char == '('):
-                current.l_child = Node(None)
-                current.l_child.parent = current
-                current = current.l_child
+            if(char == '('):
+                in_par += 1
+                rk.append(-1)                
 
             elif(char == ')'):
-                current = current.parent
+                in_par -= 1
+                rk.append(-1)
+            
+            elif(char in self.nums):
+                rk.append(0)
+                
+            elif(char in self.ops):
+                rk.append(in_par*100 + self.ops.get(char))
+        print(rk)
+        return rk    
 
-            elif(char in self.funcs):
-                current.content = char
+    def split(self, node, expr, rk):
+        min = 0
+        for i in range(len(expr)):
+            if(rk[i] < rk[min]):
+                min = i
 
-            elif(char in self.ops):           
-
-                if(current.content):    # current node not empty
-                    new_node = Node(char)
-                    # current node has lower precedence -> new node is child of current node
-                    if(self.ops.get(char) > self.ops.get(current.content) ):   
-                        new_node.l_child, current.r_child.parent = current.r_child, new_node
-                        current.r_child, new_node.parent = new_node, current
-                    # current node has higher precedence -> new node is parent of current node
-                    else:
-                        if(current.parent != None):
-                            new_node.parent = current.parent
-                            if(current.parent.l_child == current):
-                               current.parent.l_child = new_node
-                            else:
-                                current.parent.r_child = new_node
-                        else:
-                            self.root = new_node
-                        new_node.l_child, current.parent = current, new_node   
-                    current = new_node
-
-                else:                    # current node is empty -> fill in operand
-                    current.content = char
-                    
-                current.r_child = Node(None)
-                current.r_child.parent = current
-                current = current.r_child
-            i+=1
-
-        if(self.root.content == None):
-            self.root.content = 's'
-
-    
-    def eval(self, node):
-        cmd = node.content
-        #print(cmd)
-
-        if(type(cmd) == float):
-            return cmd
-
-        if(cmd == '+'):
-            return self.eval(node.l_child) + self.eval(node.r_child)
-
-        if(cmd == '-'):
-            return self.eval(node.l_child) - self.eval(node.r_child) 
-         
-        if(cmd == '*'):
-            return self.eval(node.l_child) * self.eval(node.r_child)
-
-        if(cmd == '/'):
-            return self.eval(node.l_child) / self.eval(node.r_child)
-
-        if(cmd == 'c'):    # sin(x)
-            if(self.angle):
-                return np.sin(self.eval(node.l_child)*np.pi/180)
-            return np.sin(self.eval(node.l_child))
-
-        if(cmd == 'd'):    # cos(x)
-            if(self.angle):
-                return np.cos(self.eval(node.l_child)*np.pi/180)
-            return np.cos(self.eval(node.l_child))
-
-        if(cmd == 'e'):    # tan(x)
-            if(self.angle):
-                return np.tan(self.eval(node.l_child)*np.pi/180)
-            return np.tan(self.eval(node.l_child))
-
-        if(cmd == 'f'):    # arcsin(x)
-            if(self.angle):
-                return np.arcsin(self.eval(node.l_child))/np.pi*180
-            return np.arcsin(self.eval(node.l_child))
-
-        if(cmd == 'g'):    # arccos(x)
-            if(self.angle):
-                return np.arccos(self.eval(node.l_child))/np.pi*180
-            return np.arccos(self.eval(node.l_child))
-
-        if(cmd == 'h'):    # arctan(x)
-            if(self.angle):
-                return np.arctan(self.eval(node.l_child))/np.pi*180
-            return np.arctan(self.eval(node.l_child))
-
-        if(cmd == 'i'):    # square(x)
-            return pow(self.eval(node.l_child),2)
-
-        if(cmd == 'j'):    # cube(x)
-            return pow(self.eval(node.l_child),3)
-
-        if(cmd == 'k'):    # exp(x)
-            return np.exp(self.eval(node.l_child))
-
-        if(cmd == 'l'):    # square_root(x)
-            return pow(self.eval(node.l_child),(1/2))
-
-        if(cmd == 'm'):    # cubic_root(x)
-            return pow(self.eval(node.l_child),(1/3))
-
-        if(cmd == 'n'):    # log(x)
-            return np.log10(self.eval(node.l_child))
-
-        if(cmd == 'o'):    # ln(x)
-            return np.log(self.eval(node.l_child))
-
-        if(cmd == 'p'):    # abs(x)
-            return abs(self.eval(node.l_child))
-
-        if(cmd == 'q'):    # factorial(x)
-            return math.factorial(int(abs(self.eval(node.l_child))))
+        if(rk[min] < 0):
+            return
         
-        if(cmd == 'r'):    # deg(x)
-            if(self.angle):
-                return self.eval(node.l_child)/np.pi*180
-            return self.eval(node.l_child)
-    
-        if(cmd == 's'):    # rad(x)
-            if(self.angle):
-                return self.eval(node.l_child)
-            return self.eval(node.l_child)/180*np.pi
-    
-        if(cmd == 't'):    # for void root
-            return self.eval(node.l_child)
+        node.content = rk[min]
+        print(node.content)
+        node.r_child, node.l_child = Node1(None), Node1(None)
+        node.r_child.parent, node.l_child.parent = node, node
+
+        if(expr[:min]):
+            self.split(node.r_child, expr[:min],   rk[:min])
+        if(expr[min+1:]):
+            self.split(node.l_child, expr[min+1:], rk[min+1:])
+
+    def buildTree(self,expr):
+        ranking = self.rank(expr)
+        split(self, self.root, expr, ranking)
         
 
-    def output(self):
-        return self.eval(self.root)
-        
 class Calculator1:
     def __init__(self):
         self.rnd_to = 10
-        self.angle  = True
-
+        self.angle = True 
+    
     def changeRnd(self, rnd):        # round to decimal points
         self.rnd_to = abs(int(rnd))        
 
     def changeAngle(self):     # evaluate angle in degrees/radius
         self.angle = not self.angle
+    
 
-    def calculate(self, expr):
-        tree = ParseTree(self.angle)
-        tree.buildTree(expr)
-        return round(tree.output(), self.rnd_to)
-
-#if __name__  == "__main__":
-#    test = Calculator1()
-#    expr = "c(60)*e(45)/0.2+i(4)*1.5-k(0.3)"
-#    t1 = time.time()
-#    ans = test.calculate(expr)
-#    t2 = time.time()
-#    print(ans)
-#    print("time:",t2-t1)
-
+if __name__ == "__main__":
+    tree = ParseTree1(True)
+    expr = "1+2*3"
+    tree.split(tree.root, expr, tree.rank(expr))
 # function/operator:notation
 #   a:   pi
 #   b:   e
@@ -241,7 +113,6 @@ class Calculator1:
 #   q(): factorial()
 #   r(): deg()
 #   s(): rad()
-#   t(): special function for void root
 #   +:   addition
 #   -:   subtraction
 #   *:   multiplication
