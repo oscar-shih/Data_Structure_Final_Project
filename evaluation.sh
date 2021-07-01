b505c9c504d9792d78739b1cb1b3da6df1e93ca4
Shih Min-Han <73226430+oscar-shih@users.noreply.github.com>
2021-07-01 22:21:15 +0800

    Add files via upload

--- evaluation.sh
+++ evaluation.sh
@@ -2,8 +2,8 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 echo "==evaluating correctness=="
-for i in $(seq 1 2); do
-    python shunting_yard.py --input test_data/input_${i}.txt --output test_data/output_${i}.txt
+for i in $(seq 0 4); do
+    python shunting_yard.py --input test_data/input_${i}.txt --output output_${i}.txt
     dline_py=$(diff output_${i}.txt test_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
     if [ "${dline_py}" == "0" ] && [ -f output_${i}.txt ] && [ -f test_ans/golden_${i}.txt ] ; then
         echo -e "${GREEN}shunting_yard.py is correct in test case: input_${i}.txt${NC}"
