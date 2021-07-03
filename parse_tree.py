0ab39e1ca4ab81edca514d7bb3728800f2a897e0
Beer-and-Skittles <jessicahu1012@gmail.com>
2021-07-03 22:25:58 +0800

    update

--- parse_tree.py
+++ parse_tree.py
@@ -16,6 +16,7 @@ class Node1:
 
 class ParseTree1:
     def __init__(self, a):
+        #print("self.angle is",a)
         self.angle = a
         self.nums = "0123456789."
         self.codes = "ab"
@@ -92,81 +93,107 @@ class ParseTree1:
             self.split(node.r_child, expr[max+1:], rk[max+1:])
 
     def merge(self, node):
-   
-        if(node.content == '+'):
-            return self.merge(node.r_child) + self.merge(node.l_child)
-
-        if(node.content == '-'):
-            return self.merge(node.r_child) - self.merge(node.l_child)
-
-        if(node.content == '*'):
-            return self.merge(node.r_child) * self.merge(node.l_child)
-
-        if(node.content == '/'):
-            return self.merge(node.r_child) / self.merge(node.l_child)
-
-        if(node.content == '^'):
-            return pow(self.merge(node.r_child), self.merge(node.l_child))
-        
-        if(node.content == 'c'):    # sin(x)
-            if(self.angle):
-                return np.sin(self.merge(node.l_child)*np.pi/180)
-            return np.sin(self.merge(node.l_child))
-
-        if(node.content == 'd'):    # cos(x)
-            if(self.angle):
-                return np.cos(self.merge(node.l_child)*np.pi/180)
-            return np.cos(self.merge(node.l_child))
-
-        if(node.content == 'e'):    # tan(x)
-            if(self.angle):
-                return np.tan(self.merge(node.l_child)*np.pi/180)
-            return np.tan(self.merge(node.l_child))
-
-        if(node.content == 'f'):    # arcsin(x)
-            if(self.angle):
-                return np.arcsin(self.merge(node.l_child))/np.pi*180
-            return np.arcsin(self.merge(node.l_child))
-
-        if(node.content == 'g'):    # arccos(x)
-            if(self.angle):
-                return np.arccos(self.merge(node.l_child))/np.pi*180
-            return np.arccos(self.merge(node.l_child))
-
-        if(node.content == 'h'):    # arctan(x)
-            if(self.angle):
-                return np.arctan(self.merge(node.l_child))/np.pi*180
-            return np.arctan(self.merge(node.l_child))
-
-        if(node.content == 'i'):    # square(x)
-            return pow(self.merge(node.l_child),2)
-
-        if(node.content == 'j'):    # cube(x)
-            return pow(self.merge(node.l_child),3)
-
-        if(node.content == 'k'):    # exp(x)
-            return np.exp(self.merge(node.l_child))
-
-        if(node.content == 'l'):    # square_root(x)
-            return pow(self.merge(node.l_child),(1/2))
-
-        if(node.content == 'm'):    # cubic_root(x)
-            return pow(self.merge(node.l_child),(1/3))
-
-        if(node.content == 'n'):    # log(x)
-            return np.log10(self.merge(node.l_child))
-
-        if(node.content == 'o'):    # ln(x)
-            return np.log(self.merge(node.l_child))
-
-        if(node.content == 'p'):    # abs(x)
-            return abs(self.merge(node.l_child))
-
-        if(node.content == 'q'):    # factorial(x)
-            return math.factorial(int(abs(self.merge(node.l_child))))
-        
-        else:
+        if(type(node.content) == float):
             return node.content
+
+        elif(str(node.content) in "+-*/^"):
+
+            a = self.merge(node.r_child)
+            b = self.merge(node.l_child)
+            c = None
+
+            if(node.content == '+'):
+                c = a + b
+
+            elif(node.content == '-'):
+                c = a - b
+
+            elif(node.content == '*'):
+                c = a * b
+
+            elif(node.content == '/'):
+                c = a / b
+
+            elif(node.content == '^'):
+                c = a ** b
+            
+            #print(c,"=",a,node.content,b)
+            return c
+    
+        elif(node.content in self.ops):
+   
+            A = self.merge(node.l_child)
+            c = None
+
+            if(node.content == 'c'):    # sin(x)
+                if(self.angle):
+                    c =  np.sin(A*np.pi/180)
+                else:
+                    c =  np.sin(A)
+
+            elif(node.content == 'd'):    # cos(x)
+                if(self.angle):
+                    c =  np.cos(A*np.pi/180)
+                else:
+                    c =  np.cos(A)
+
+            elif(node.content == 'e'):    # tan(x)
+                if(self.angle):
+                    c =  np.tan(A*np.pi/180)
+                else:
+                    c =  np.tan(A)
+    
+            elif(node.content == 'f'):    # arcsin(x)
+                #if(self.angle):
+                #    c =  np.arcsin(A)/np.pi*180
+                #else:
+                #    c =  np.arcsin(A)
+                c = np.arcsin(A)
+
+            elif(node.content == 'g'):    # arccos(x)
+                #if(self.angle):
+                #    c =  np.arccos(A)/np.pi*180
+                #else:
+                #    c =  np.arccos(A)
+                c = np.arccos(A)
+
+            elif(node.content == 'h'):    # arctan(x)
+                #if(self.angle):
+                #    c =  np.arctan(A)/np.pi*180
+                #else:
+                #    c =  np.arctan(A)
+                c = np.arctan(A)
+
+            elif(node.content == 'i'):    # square(x)
+                c =  pow(A,2)
+
+            elif(node.content == 'j'):    # cube(x)
+                c =  pow(A,3)
+
+            elif(node.content == 'k'):    # exp(x)
+                c =  np.exp(A)
+
+            elif(node.content == 'l'):    # square_root(x)
+                c =  pow(A,(1/2))
+
+            elif(node.content == 'm'):    # cubic_root(x)
+                c =  pow(A,(1/3))
+
+            elif(node.content == 'n'):    # log(x)
+                c =  np.log10(A)
+
+            elif(node.content == 'o'):    # ln(x)
+                c =  np.log(A)
+
+            elif(node.content == 'p'):    # abs(x)
+                c =  abs(A)
+
+            elif(node.content == 'q'):    # factorial(x)
+                c =  math.factorial(int(abs(A)))
+            
+            #print(c,"=",node.content,"(",A,")")
+            return c
+    
 
 
     def output(self,expr):
@@ -178,7 +205,7 @@ class ParseTree1:
 class Calculator1:
     def __init__(self):
         self.rnd_to = 3
-        self.angle = True 
+        self.angle = True
     
     def changeRnd(self, rnd):        # round to decimal points
         self.rnd_to = abs(int(rnd))        
@@ -202,22 +229,48 @@ class Calculator1:
                 output.write('\n')
             t2 = time.time()
             print('time:'+str(t2-t1))
-if __name__ == "__main__":
-    # test = Calculator1()
-    # #expr = "c(60)*e(45)/0.2+i(4)*1.5-k(0.3)"
-    # expr = "2^3^2+(2*3-2^3^2)"
 
-    # t1 = time.time()
-    # ans = test.calculate(expr)
-    # t2 = time.time()
-    # print(ans)
-    # print("time:",t2-t1)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, default='./correctness/correct_1.txt',help="Input file root")
-    parser.add_argument("--output", type=str, default='./ouput_1.txt', help="Output file root")
-    args = parser.parse_args()
-    Cal = Calculator1()
-    Cal.main(args.input, args.output) 
+if __name__ == "__main__":
+
+    test = Calculator1()
+    expr = "c(45)"        
+
+    t1 = time.time()
+    ans = test.calculate(expr)
+    t2 = time.time()
+    print(ans)
+    print("time:",t2-t1)
+    print()
+
+    #test = Calculator1()
+    #expr = [
+    #"i(11)*p(10^2-2^10)+l(100)/a-j(10)-f(0.8)*h(37)*h(37)",
+    #"f(0.8)/o(8*b^b)-o(8*b^b)*q(15)/f(0.8)*p(10^2-2^10)/q(15)*m(17)",
+    #"i(11)-b-m(17)-h(37)*d(60)/b+l(100)+i(11)",
+    #"a-k(2)*g(0.3)*j(10)/k(2)-l(100)/g(0.3)+k(2)",
+    #"a/o(8*b^b)-e(80)-f(0.8)+f(0.8)+q(5)-i(11)+l(100)",
+    #"m(17)*e(80)+d(60)-q(15)/j(10)*g(0.3)-n(3*10^7)*b",
+    #"e(80)-e(80)/g(0.3)*k(2)*f(0.8)-j(10)-o(8*b^b)/g(0.3)",
+    #"d(60)+d(60)/b*h(37)+c(30)+b+d(60)/b",
+    #"a-o(8*b^b)/f(0.8)/c(30)+g(0.3)-g(0.3)-k(2)*l(100)",
+    #"i(11)-h(37)*m(17)/m(17)+j(10)-i(11)-h(37)*i(11)",
+    #"q(9)*n(3*10^7)+i(11)-d(60)*o(8*b^b)*d(60)+l(100)*d(60)",
+    #"c(30)/p(10^2-2^10)+k(2)/a-f(0.8)*f(0.8)*f(0.8)*a",
+    #"j(10)+p(10^2-2^10)+k(2)*c(30)+n(3*10^7)+d(60)*e(80)+m(17)",
+    #"f(0.8)+c(30)/f(0.8)/b+l(100)+l(100)/f(0.8)/a",
+    #"b/n(3*10^7)+i(11)-m(17)/g(0.3)+d(60)+m(17)/i(11)"
+    #]
+        
+    #for x in expr:
+    #    print(test.calculate(x))
+
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("--input", type=str, default='./correctness/correct_1.txt',help="Input file root")
+    #parser.add_argument("--output", type=str, default='./ouput_1.txt', help="Output file root")
+    #args = parser.parse_args()
+    #Cal = Calculator1()
+    #Cal.main(args.input, args.output) 
+
 # function/operator:notation
 #   a:   pi
 #   b:   e
