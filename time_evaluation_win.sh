0393801fb5ed91342a637609b43c7e7af04dc4d0
Beer-and-Skittles <53812829+Beer-and-Skittles@users.noreply.github.com>
2021-07-04 21:38:13 +0800

    time_evaluation_win.sh

--- time_evaluation_win.sh
+++ time_evaluation_win.sh
@@ -17,13 +17,3 @@ for i in $(seq 1 5); do
     python shunting_yard.py --input time_data/input_${i}.txt --output sy_time_output_${i}.txt
     echo -e "${GREEN}shunting_yard.py time in test case: input_${i}.txt${NC}"
 done
-
-for i in $(seq 1 5); do
-    # python3 shunting_yard.py --input time_data/input_${i}.txt --output sy_time_output_${i}.txt
-    dline_py=$(diff sy_time_output_${i}.txt pt_time_output_${i}.txt |  wc | awk -F ' ' '{print $1}')
-    if [ "${dline_py}" == "0" ] && [ -f sy_time_output_${i}.txt ] && [ -f pt_time_output_${i}.txt ] ; then
-        echo -e "${GREEN}shunting_yard.py is same with parse_tree.py in test case: input_${i}.txt${NC}"
-    else
-        echo -e "${RED}shunting_yard.py is different from parse_tree.py in test case: input_${i}.txt${NC}"
-    fi
-done
