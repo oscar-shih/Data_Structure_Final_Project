44c520d46e52131cb31856c033008e216e07106e
Beer-and-Skittles <53812829+Beer-and-Skittles@users.noreply.github.com>
2021-07-04 20:32:42 +0800

    sync

--- time_evaluation.sh
+++ time_evaluation.sh
@@ -8,12 +8,21 @@ for i in $(seq 1 5); do
     echo -e "${GREEN}parse_tree.py time in test case: input_${i}.txt${NC}"
 done
 
-for i in $(seq 1 5); do
-    python3 dp.py --input time_data/input_${i}.txt --output dp_time_output_${i}.txt
-    echo -e "${GREEN}dp.py time in test case: input_${i}.txt${NC}"
-done
-
+# for i in $(seq 1 5); do
+#     python3 parse_tree2.py --input time_data/input_${i}.txt --output pt2_time_output_${i}.txt
+#     echo -e "${GREEN}parse_tree2.py time in test case: input_${i}.txt${NC}"
+# done
 for i in $(seq 1 5); do
     python3 shunting_yard.py --input time_data/input_${i}.txt --output sy_time_output_${i}.txt
     echo -e "${GREEN}shunting_yard.py time in test case: input_${i}.txt${NC}"
+done
+
+for i in $(seq 1 5); do
+    # python3 shunting_yard.py --input time_data/input_${i}.txt --output sy_time_output_${i}.txt
+    dline_py=$(diff sy_time_output_${i}.txt pt_time_output_${i}.txt |  wc | awk -F ' ' '{print $1}')
+    if [ "${dline_py}" == "0" ] && [ -f sy_time_output_${i}.txt ] && [ -f pt_time_output_${i}.txt ] ; then
+        echo -e "${GREEN}shunting_yard.py is same with parse_tree.py in test case: input_${i}.txt${NC}"
+    else
+        echo -e "${RED}shunting_yard.py is different from parse_tree.py in test case: input_${i}.txt${NC}"
+    fi
 done
