bfdcd834f1ae49d8e141530caf359b4d35bf6917
Beer-and-Skittles <53812829+Beer-and-Skittles@users.noreply.github.com>
2021-07-04 21:34:00 +0800

    evaluation_win.sh

--- evaluation_win.sh
+++ evaluation_win.sh
@@ -5,8 +5,8 @@ NC='\033[0m' # No Color
 echo "==evaluating correctness=="
 for i in $(seq 1 5); do
     python parse_tree.py --input correctness/correct_${i}.txt --output pt_output_${i}.txt
-    dline_py=$(diff pt_output_${i}.txt correct_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
-    if [ "${dline_py}" == "0" ] && [ -f pt_output_${i}.txt ] && [ -f correct_ans/golden_${i}.txt ] ; then
+    dline_py=$(diff pt_output_${i}.txt correct_ans_win/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
+    if [ "${dline_py}" == "0" ] && [ -f pt_output_${i}.txt ] && [ -f correct_ans_win/golden_${i}.txt ] ; then
         echo -e "${GREEN}parse_tree.py is correct in test case: input_${i}.txt${NC}"
     else
         echo -e "${RED}parse_tree.py is incorrect in test case: input_${i}.txt${NC}"
@@ -15,8 +15,8 @@ done
 
 for i in $(seq 1 5); do
     python dp.py --input correctness/correct_${i}.txt --output dp_output_${i}.txt
-    dline_py=$(diff dp_output_${i}.txt correct_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
-    if [ "${dline_py}" == "0" ] && [ -f dp_output_${i}.txt ] && [ -f correct_ans/golden_${i}.txt ] ; then
+    dline_py=$(diff dp_output_${i}.txt correct_ans_win/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
+    if [ "${dline_py}" == "0" ] && [ -f dp_output_${i}.txt ] && [ -f correct_ans_win/golden_${i}.txt ] ; then
         echo -e "${GREEN}dp.py is correct in test case: input_${i}.txt${NC}"
     else
         echo -e "${RED}dp.py is incorrect in test case: input_${i}.txt${NC}"
@@ -25,8 +25,8 @@ done
 
 for i in $(seq 1 5); do
     python shunting_yard.py --input correctness/correct_${i}.txt --output sy_output_${i}.txt
-    dline_py=$(diff sy_output_${i}.txt correct_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
-    if [ "${dline_py}" == "0" ] && [ -f sy_output_${i}.txt ] && [ -f correct_ans/golden_${i}.txt ] ; then
+    dline_py=$(diff sy_output_${i}.txt correct_ans_win/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
+    if [ "${dline_py}" == "0" ] && [ -f sy_output_${i}.txt ] && [ -f correct_ans_win/golden_${i}.txt ] ; then
         echo -e "${GREEN}shunting_yard.py is correct in test case: input_${i}.txt${NC}"
     else
         echo -e "${RED}shunting_yard.py is incorrect in test case: input_${i}.txt${NC}"
