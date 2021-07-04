dfecea4da2eb560165f6b42e77e6b6982a29aa1b
Beer-and-Skittles <53812829+Beer-and-Skittles@users.noreply.github.com>
2021-07-04 20:29:59 +0800

    sync

--- evaluation.sh
+++ evaluation.sh
@@ -4,7 +4,7 @@ NC='\033[0m' # No Color
 
 echo "==evaluating correctness=="
 for i in $(seq 1 5); do
-    python3 parse_tree.py --input correctness/correct_${i}.txt --output pt_output_${i}.txt
+    python parse_tree.py --input correctness/correct_${i}.txt --output pt_output_${i}.txt
     dline_py=$(diff pt_output_${i}.txt correct_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
     if [ "${dline_py}" == "0" ] && [ -f pt_output_${i}.txt ] && [ -f correct_ans/golden_${i}.txt ] ; then
         echo -e "${GREEN}parse_tree.py is correct in test case: input_${i}.txt${NC}"
@@ -14,7 +14,7 @@ for i in $(seq 1 5); do
 done
 
 for i in $(seq 1 5); do
-    python3 dp.py --input correctness/correct_${i}.txt --output dp_output_${i}.txt
+    python dp.py --input correctness/correct_${i}.txt --output dp_output_${i}.txt
     dline_py=$(diff dp_output_${i}.txt correct_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
     if [ "${dline_py}" == "0" ] && [ -f dp_output_${i}.txt ] && [ -f correct_ans/golden_${i}.txt ] ; then
         echo -e "${GREEN}dp.py is correct in test case: input_${i}.txt${NC}"
@@ -24,7 +24,7 @@ for i in $(seq 1 5); do
 done
 
 for i in $(seq 1 5); do
-    python3 shunting_yard.py --input correctness/correct_${i}.txt --output sy_output_${i}.txt
+    python shunting_yard.py --input correctness/correct_${i}.txt --output sy_output_${i}.txt
     dline_py=$(diff sy_output_${i}.txt correct_ans/golden_${i}.txt |  wc | awk -F ' ' '{print $1}')
     if [ "${dline_py}" == "0" ] && [ -f sy_output_${i}.txt ] && [ -f correct_ans/golden_${i}.txt ] ; then
         echo -e "${GREEN}shunting_yard.py is correct in test case: input_${i}.txt${NC}"
